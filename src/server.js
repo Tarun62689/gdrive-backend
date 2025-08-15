6345fb639dc4950cfe2ce71f38550d3e6d511f9e
TarunAS <taruntarun62689@example.com>
2025-08-15 10:19:17 +0530

    b

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 import app from './app.js';
 import 'dotenv/config';
+
 import uploadRoutes from './routes/upload.js';
 import folderRoutes from './routes/folderRoutes.js';
 import fileRoutes from './routes/fileRoutes.js';
-import serverless from 'serverless-http';
 import userRoutes from './routes/userRoutes.js';
 
 const PORT = process.env.PORT || 5000;
-const baseUrl = process.env.BASE_URL || `http://localhost:${PORT}`;
 
 // Routes
 app.use('/api', uploadRoutes);
@@ -15,13 +14,7 @@ app.use('/api', folderRoutes);
 app.use('/api/files', fileRoutes);
 app.use('/api/user', userRoutes);
 
-
-// Local dev mode only
-if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    console.log(`✅ Server running locally at ${baseUrl}`);
-  });
-}
-
-// ✅ Export for Vercel serverless
-export default serverless(app);
+// Start the server (works for Render)
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
